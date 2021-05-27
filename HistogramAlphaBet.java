6248a8e052eb49c5a79f40af9c55e843043c43e7
Daniel Rosenthal <57165307+danrose499@users.noreply.github.com>
2021-05-27 11:00:59 -0400

    Update HistogramAlphaBet.java

--- HistogramAlphaBet.java
+++ HistogramAlphaBet.java
@@ -1,23 +1,21 @@
 package sample;
 
-import javafx.scene.canvas.GraphicsContext;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.*;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.chart.*;
-import java.util.Map;
+
 import static java.util.stream.Collectors.toMap;
 
 
 public class HistogramAlphaBet {
     String filename; // file name of file to be searched
     int totalChars;  // Total characters in file to be searched
-    Map<Character, Integer> sortedFrequency; // Sorted frequency map of characters in the file
+    Map<String, Double> sortedFrequency; // Sorted frequency map of characters in the file
 
     HistogramAlphaBet(String filename) throws IOException {
         this.filename = filename;
@@ -30,46 +28,37 @@ public class HistogramAlphaBet {
         String s = actual.replaceAll("[^a-zA-Z]", "").toLowerCase();
         totalChars = s.length();
 
-        Map<Character, Integer> frequency = new HashMap<>();
-
+        Map<String, Double> freq2 = new HashMap<>();
         for(int i = 0; i < totalChars; i++) {
-            Character Ch = s.charAt(i);
-            incrementFrequency(frequency, Ch);
+            String Ch = Character.toString(s.charAt(i));
+            incrementFrequency(freq2, Ch);
         }
 
-        this.sortedFrequency = frequency
+        this.sortedFrequency = freq2
                 .entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
-
-
     }
 
-    public static <K> void incrementFrequency(Map<K, Integer> m, K Key) {
-        m.putIfAbsent(Key, 0);
-        m.put(Key, m.get(Key) + 1);
+    public static <K> void incrementFrequency(Map<K, Double> m, K Key) {
+        m.putIfAbsent(Key, 0.00);
+        m.put(Key, m.get(Key) + 1.00);
     }
 
     public PieChart drawPieChart(int SlicesToPrint) {
-//        int cWidth = (int) GC.getCanvas().getWidth();
-//        int cHeight = (int) GC.getCanvas().getHeight();
-//        double d = (3.0 * (Math.min(cWidth, cHeight) / 4.0))-50;
-        //MyPoint arcPoint = new MyPoint(cWidth / 2 - (int) d / 2, cHeight / 2 - (int) d / 2);
-        //PieChart charChart = new PieChart(arcPoint, d);
-        //charChart.draw(GC, totalChars, SlicesToPrint, sortedFrequency);
-
-
-        ObservableList<PieChart.Data> pieChartData = null;
-        for (Map.Entry<Character, Integer> mapElement : sortedFrequency.entrySet()) {
-            //if(SlicesToPrint-- > 0)
-                pieChartData.add(new PieChart.Data(String.valueOf(mapElement.getKey()), mapElement.getValue()));
+        //ObservableList<PieChart.Data> pieChartData = null;
+        ObservableList pieChartData = FXCollections.observableArrayList();
+        for (Map.Entry<String, Double> mapElement : sortedFrequency.entrySet()) {
+            if(SlicesToPrint-- > 0)
+                pieChartData.add(new PieChart.Data(mapElement.getKey(), mapElement.getValue()));
         }
         return new PieChart(pieChartData);
+
     }
 
-//    public void recount(String newFile) throws IOException {
-//        this.filename = newFile;
-//        getMap();
-//    }
+    public void updateFile(String newFile) throws IOException {
+        this.filename = newFile;
+        getMap();
+    }
 }
